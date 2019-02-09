b4ad4d1eab8c2f297dda7863fb91ea1c57618245
Joshisaka1004 <joshi1004@gmail.com>
2019-02-09 22:43:08 +0100

    Initial Setup with ToDoListVC and DetailVC for certain to do list items

---
+++ CoreData_Realm_190209/DetailVC.swift
@@ -0,0 +1,17 @@
+//
+//  DetailVC.swift
+//  CoreData_Realm_190209
+//
+//  Created by Joachim Vetter on 09.02.19.
+//  Copyright © 2019 Joachim Vetter. All rights reserved.
+//
+
+import UIKit
+
+class DetailVC: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
