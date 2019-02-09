3425fc034fe56afb1692f0828e8b78eb488db89f
Joshisaka1004 <joshi1004@gmail.com>
2019-02-09 22:36:20 +0100

    Initial Commit

---
+++ CoreData_Realm_190209/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreData_Realm_190209
+//
+//  Created by Joachim Vetter on 09.02.19.
+//  Copyright © 2019 Joachim Vetter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
